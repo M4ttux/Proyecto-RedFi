7ef836ffc36dc7dd222541f42c5dda625c200f6d
Matias Neto <mattux_@hotmail.com>
2025-05-14 16:51:12 -0300

    Se mejoró la vista Inicio (aun se sigue trabajando)

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,36 +1,3 @@
-/* import { Link } from "react-router-dom";
-
-const HeroSection = () => {
-    return (
-        <section className="relative w-full h-[60vh] flex items-center justify-center text-center overflow-hidden bg-neutral-900">
-        <div className="absolute inset-0">
-            <img
-            src="/imgs/hero-placeholder.jpg" // tu imagen real
-            alt="Mapa de conexiones"
-            className="w-full h-full object-cover opacity-20"
-            />
-        </div>
-
-        <div className="relative z-10 px-6 max-w-3xl mx-auto">
-            <h1 className="text-4xl md:text-5xl font-bold text-primario drop-shadow-lg">
-            Encontrá el mejor Internet para vos
-            </h1>
-            <p className="mt-4 text-lg text-texto drop-shadow-sm">
-            Compará proveedores, leé experiencias reales y elegí con confianza.
-            </p>
-            <Link
-            to="/mapa"
-            className="inline-block mt-6 px-6 py-3 bg-primario text-white rounded hover:bg-acento"
-            >
-            Explorar Mapa
-            </Link>
-        </div>
-        </section>
-    );
-};
-
-export default HeroSection; */
-
 import { Link } from "react-router-dom";
 
 const HeroSection = () => {
@@ -44,8 +11,8 @@ const HeroSection = () => {
       />
 
       {/* Capa de texto */}
-      <div className="relative z-10 text-center px-6 max-w-3xl">
-        <h1 className="text-4xl md:text-5xl font-bold text-primario drop-shadow-md">
+      <div className="relative z-10 text-center px-6 max-w-8xl">
+        <h1 className="text-6xl font-bold text-texto drop-shadow-md">
           Encontrá el mejor proveedor de Internet según tu zona
         </h1>
         <p className="mt-4 text-lg text-texto drop-shadow">
@@ -53,7 +20,7 @@ const HeroSection = () => {
         </p>
         <Link
           to="/mapa"
-          className="inline-block mt-6 px-6 py-3 bg-primario text-white rounded hover:bg-acento transition"
+          className="inline-block mt-6 px-6 py-3 bg-primario text-white rounded-full hover:bg-acento transition font-bold"
         >
           Ver Mapa
         </Link>
