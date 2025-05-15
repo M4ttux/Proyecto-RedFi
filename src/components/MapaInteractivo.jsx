90e05a838830de5f8146cc14b877d8535b03946c
Matias Neto <mattux_@hotmail.com>
2025-05-15 17:15:20 -0300

    cambio de estilo al mapa | borracion de packages que estaban alpedo

--- src/components/MapaInteractivo.jsx
+++ src/components/MapaInteractivo.jsx
@@ -1,82 +1,3 @@
-/* import maplibregl from "maplibre-gl";
-import "maplibre-gl/dist/maplibre-gl.css";
-import { useEffect, useRef } from "react";
-import { obtenerProveedores } from "../services/proveedorService";
-
-const MapaInteractivo = () => {
-  const mapContainer = useRef(null);
-  const bounds = [ // limitar a Corrientes
-    [-60.9, -31.1],
-    [-54.1, -26.1]
-  ];
-
-  useEffect(() => {
-    const map = new maplibregl.Map({
-      container: mapContainer.current,
-      style: "/map-styles/americana.json",
-      center: [-58.95, -28.65], 
-      zoom: 2,
-      maxBounds: bounds
-    });
-
-    map.addControl(new maplibregl.NavigationControl(), "top-right");
-
-    map.on("load", async () => {
-      try {
-        const proveedores = await obtenerProveedores();
-        console.log("📦 Proveedores:", proveedores);
-        
-
-        proveedores.forEach((prov) => {
-          if (!prov.zonas || !prov.zonas.geom) return;
-
-          map.addSource(`zona-${prov.id}`, {
-            type: "geojson",
-            data: {
-              type: "Feature",
-              geometry: prov.zonas.geom,
-              properties: {},
-            },
-          });
-
-          map.addLayer({
-            id: `fill-${prov.id}`,
-            type: "fill",
-            source: `zona-${prov.id}`,
-            paint: {
-              "fill-color": prov.color || "#888888",
-              "fill-opacity": 0.4,
-            },
-          });
-
-          map.addLayer({
-            id: `line-${prov.id}`,
-            type: "line",
-            source: `zona-${prov.id}`,
-            paint: {
-              "line-color": prov.color || "#000000",
-              "line-width": 2,
-            },
-          });
-        });
-      } catch (error) {
-        console.error("Error al cargar proveedores:", error);
-      }
-    });
-
-    return () => map.remove();
-  }, []);
-
-  return (
-    <div className="h-full w-full">
-      <div ref={mapContainer} className="w-full h-full" />
-    </div>
-  );
-};
-
-export default MapaInteractivo;
- */
-
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { useEffect, useRef, useState } from "react";
@@ -98,7 +19,7 @@ const MapaInteractivo = () => {
   useEffect(() => {
     const map = new maplibregl.Map({
       container: mapContainer.current,
-      style: "/map-styles/americana.json",
+      style: "https://api.maptiler.com/maps/streets-v2-dark/style.json?key=911tGzxLSAMvhDUnyhXL",
       center: [-58.95, -28.65],
       zoom: 2,
       maxBounds: bounds,
